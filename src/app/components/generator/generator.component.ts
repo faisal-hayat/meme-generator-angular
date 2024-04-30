67630d4ee0e40b1d5de488a01367e83c4cbd7524
Muhammad Faisal Hayat <faisal.hayat@routetrading.co.uk>
2024-04-30 20:43:05 +0500

    generator component has been added

---
+++ src/app/components/generator/generator.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-generator',
+  standalone: true,
+  imports: [],
+  templateUrl: './generator.component.html',
+  styleUrl: './generator.component.css'
+})
+export class GeneratorComponent {
+
+}
