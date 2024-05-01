074ffac3ed49c6c84946d90e04d96f1a8b77be2d
Muhammad Faisal Hayat <faisal.hayat@routetrading.co.uk>
2024-05-01 13:58:21 +0500

    bootstrap is working

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
+  
+  //#region constructor
+  constructor(private toastrService: ToastrService){
+  }
+  //#endregion
+  
 }
